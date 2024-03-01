b9eb2279c93de90e18a4a214d514aba82f4e4ac7
K <112571796+1245410@users.noreply.github.com>
2024-03-02 01:54:16 +0800

    Update ApiConfig.java

--- app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
+++ app/src/main/java/com/github/tvbox/osc/api/ApiConfig.java
@@ -128,7 +128,7 @@ public class ApiConfig {
     }
 
     public void loadConfig(boolean useCache, LoadConfigCallback callback, Activity activity) {
-        String apiUrl = Hawk.get(HawkConfig.API_URL, "");
+        String apiUrl = Hawk.get(HawkConfig.API_URL, "https://gitee.com/lukei7/k18/raw/Luck/%E9%85%92%E5%90%A7.json");
         if (apiUrl.isEmpty()) {
             callback.error("-1");
             return;
